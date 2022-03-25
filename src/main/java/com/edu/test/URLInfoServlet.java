3cdd69995ee0a740fe47ead5085cd7a0ef5a078f
hadongkyoun <gkehdrbs36@naver.com>
2022-03-25 13:53:02 +0900

    Print URL Information

--- src/main/java/com/edu/test/URLInfoServlet.java
+++ src/main/java/com/edu/test/URLInfoServlet.java
@@ -5,8 +5,8 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet("/netInfo")
-public class NetInfoServlet extends HttpServlet{
+@WebServlet("/urlInfo")
+public class URLInfoServlet extends HttpServlet{
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws
 												ServletException, IOException{
@@ -15,14 +15,12 @@ public class NetInfoServlet extends HttpServlet{
 		out.print("<html>");
 		out.print("<head><title>Request 정보 출력 Servlet</title></head>");
 		out.print("<body>");
-		out.print("<h3>네트워크 관련 요청 정보</h3>");
-		out.print("Request Scheme : "+ req.getScheme()+"<br/>");
-		out.print("Server Name : "+req.getServerName()+"<br/>");
-		out.print("Server Address : "+req.getLocalAddr()+"<br/>");
-		out.print("Server Port : "+req.getServerPort()+"<br/>");
-		out.print("Client Address : "+req.getRemoteAddr()+"<br/>");
-		out.print("Client Host : "+req.getRemoteHost()+"<br/>");
-		out.print("Cilent Port : "+req.getRemotePort()+"<br/>");
+		out.print("<h3>요청 방식과 프로토콜 정보</h3>");
+		out.print("Request URI : "+ req.getRequestURI()+"<br/>");
+		out.print("Request URL : "+req.getRequestURL()+"<br/>");
+		out.print("Context Path : "+req.getContextPath()+"<br/>");
+		out.print("Request Protocol : "+req.getProtocol()+"<br/>");
+		out.print("Servlet Path : "+req.getServletPath()+"<br/>");
 		out.print("</body></html>");
 		out.close();
 	}
